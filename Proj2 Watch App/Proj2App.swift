eb84ffec5f64a507c6310ea9ef4daf0d66f06c03
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-05-02 19:08:22 +0100

    Initial Commit

---
+++ Proj2 Watch App/Proj2App.swift
@@ -0,0 +1,17 @@
+//
+//  Proj2App.swift
+//  Proj2 Watch App
+//
+//  Created by Alfie Le Feuvre on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Proj2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
